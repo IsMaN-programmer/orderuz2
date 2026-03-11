9766ecde117e997de93b764d5d8b8a38c5338a2f
IsMaN-programmer <sr.racer77@gmail.com>
2026-03-11 16:28:52 +0500

    Add files via upload

---
+++ src/data/index.ts
@@ -0,0 +1,16 @@
+import { IMAGES } from '@/assets/images';
+import { 
+  Restaurant, 
+  FoodItem, 
+  VideoFeed, 
+  User 
+} from '@/lib/index';
+
+// Demo data removed - app uses real user-generated content
+export const mockRestaurants: Restaurant[] = [];
+
+export const mockFoodItems: FoodItem[] = [];
+
+export const mockVideoFeeds: VideoFeed[] = [];
+
+export const mockUsers: User[] = [];
